32df2ef0fdeab32d15af750c7294cccef3f83ebe
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-01 23:38:10 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,5 +14,14 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, 
+    ingredients_attributes: [:name, :quantity])
   end
 end
