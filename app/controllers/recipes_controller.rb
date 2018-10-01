3214f0f3251016a65a7513356a104e03fd96d41b
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-01 23:43:50 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,20 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.new
+    @recipe.ingredients.new
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,
+    ingredients_attributes: [:name, :quantity])
+  end
+  
 end
